9940708d2b797eee819c31a16e051d5d78d5cb4a
SAIKUMAR SWARNAPUDI <117681046+Saikumar2129@users.noreply.github.com>
2026-03-03 18:07:09 +0530

    Create golden_model_axi.py

---
+++ SiliAXI/golden_model_axi.py
@@ -0,0 +1,2 @@
+#this code is not availble for the public.
+#If you want to know about this model kindly contact " sharewithsaikumar@gmail.com with the subject : "SiliAgent-AI : Golden Models : your organization name" 
